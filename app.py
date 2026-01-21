7658aab085e05def3720ae0076b8526418260b99
TestUser <testuser@test.com>
2026-01-21 10:35:44 +0100

    Update greeting message for code review demo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello and Goodbye! NEW Testing 2222222'
+    return 'Helloo hoow are you?? very nice'
 
 
 if __name__ == '__main__':
